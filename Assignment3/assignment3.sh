5eb82c0018fb4e1d11780e9765fa86a3e75cb906
aavanderleij <a.a.van.der.leij@st.hanze.nl>
2024-06-25 17:13:13 +0200

    almost done with base functions

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python assingnment3.py
+# set input file
+input_file="/homes/aavanderleij/BDC_2024/testFiles/5reads.fastq"
+
+# decode quality score of fastq file in parallel
+awk 'NR % 4 == 0' "$input_file" | parallel -j 20  "python3 assignment3.py -d -l {}" | python3 assignment3.py -m
+
