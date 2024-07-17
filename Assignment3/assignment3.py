ce55bb8970e733bc1fa46cc3d304b066b7e9e30a
aavanderleij <a.a.van.der.leij@st.hanze.nl>
2024-07-17 16:29:24 +0200

    Removed unnecessary arguments to shorten the code and flow better with the bash script. still need to add options for multiple files.

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -14,23 +14,11 @@ import ast
 
 def argparser():
     parser = ap.ArgumentParser(description="Process quality scores and compute mean scores.")
-    mode = parser.add_mutually_exclusive_group(required=True)
 
-    mode.add_argument("-d", "--decode", action="store_true",
+    parser.add_argument("-d", "--decode", action="store_true",
                       help="Decode quality scores and print them.")
-    mode.add_argument("-m", "--mean", action="store_true",
+    parser.add_argument("-m", "--mean", action="store_true",
                       help="Compute mean scores across all quality scores.")
-
-    decode_args = parser.add_argument_group(title="Arguments when run when decoding")
-    decode_args.add_argument("-l", "--line", type=str,
-                             help="Line of quality scores to decode.")
-
-    mean_args = parser.add_argument_group(title="Arguments when run when calculating the mean and writhing output")
-    mean_args.add_argument("-o", action="store", dest="csvfile",
-                           type=ap.FileType('w', encoding='UTF-8'),
-                           required=False,
-                           help="CSV file om de output in op te slaan. Default is output naar "
-                                "terminal STDOUT")
 
     args = parser.parse_args()
 
@@ -81,32 +69,32 @@ def write_outfile(csvfile, mean_score_list):
     base_num = list(range(1, len(mean_score_list) + 1))
     # add base number by value
     final_list = list(zip(base_num, mean_score_list))
-    if csvfile is None:
-        # stdout
-        writer = csv.writer(sys.stdout)
+
+    # write into csv file
+    with open(csvfile, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
         writer.writerows(final_list)
-    else:
-        # write into csv file
-        with open(csvfile, 'a', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerows(final_list)
     return 0
 
 
 def main():
     # Get args
     args = argparser()
-    if args.decode and args.line:
-        # Decode quality scores
-        decoded_scores = decode_fastq_quality_score(args.line)
-        print(decoded_scores)
+    if args.decode:
+        # read input directly form stdin
+        for line in sys.stdin:
+
+            # Decode quality scores
+            decoded_scores = decode_fastq_quality_score(line)
+            print(decoded_scores)
 
     elif args.mean:
         # Read decoded quality scores from stdin
         decoded_lists = []
+
         for line in sys.stdin:
             line = line.strip()
-            line = line.strip("[]")  # Remove the brackets
+            line = line.strip("[]")
             decoded_list = list(map(int, line.split(',')))
             decoded_lists.append(decoded_list)
 
