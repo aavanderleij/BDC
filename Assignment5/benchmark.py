37c4139a215a783e28affb5583cafc2085fb1568
aavanderleij <a.a.van.der.leij@st.hanze.nl>
2025-01-29 16:26:09 +0100

    added benchmarking pl.join. (with one big and one small df

--- Assignment5/benchmark.py
+++ Assignment5/benchmark.py
@@ -13,6 +13,10 @@ from datetime import datetime, timezone
 from pathlib import Path
 from timeit import timeit
 
+# constands
+sqlite_file = "data/watlas-2023.sqlite"
+tag_file = 'data/tags_watlas_all.xlsx'
+
 
 def get_subset_query():
     """
@@ -39,9 +43,9 @@ def get_subset_query():
     return query
 
 
-def get_sql_data():
+def get_sql_data(sqlite_file_path=sqlite_file):
     # get uri
-    sqlite_file_path = Path("data/watlas-2023.sqlite").absolute()
+    sqlite_file_path = Path(sqlite_file_path).absolute()
 
     db_uri = f"sqlite:///{sqlite_file_path.as_posix()}"
 
@@ -59,18 +63,63 @@ def get_sql_data():
     #         """
     # run query
     watlas_df = pl.read_database_uri(query, db_uri)
-    # get shape
-    # print(watlas_df.head())
-    # print(watlas_df.shape)
-    # print(watlas_df.estimated_size("mb"))
+
+    # add short tag number column
+    watlas_df = watlas_df.with_columns([
+        pl.col("TAG"),
+        pl.col("TAG").cast(pl.String).str.slice(-4).cast(pl.Int64).alias("tag")])
+
+    return watlas_df
 
 
+def read_tag_file(tag_csv_path=tag_file):
+    """
+    Read a xlsx file and return a polars dataframe
+    Returns:
+    """
 
+    tag_csv_path = Path(tag_csv_path)
+
+    if tag_csv_path.exists():
+
+        tags_df = pl.read_excel(tag_csv_path)
+        # remove any spaces in species (bar-tailed godwit -> bar-tailed_godwit)
+        tags_df = tags_df.with_columns(pl.col("species").str.replace(" ", "_").alias("species"))
+    else:
+        sys.exit(f"tag file not found: {tag_csv_path.absolute()}. Check config file")
+
+    return tags_df
+
+
+def join_tags(watlas_df, tags_df=read_tag_file()):
+    """
+    join a big dataframe with a smaller meta data frame to get species column
+    Returns:
+
+    """
+    watlas_df = watlas_df.join(tags_df.select(["tag", "species"]), on="tag", how="left")
+
+    return watlas_df
 
 
 def main():
-    time_get_data = timeit(lambda: get_sql_data(), number=5)
+    # get data + query warmup
+    watlas_df = get_sql_data()
+
+    # benchmark reading SQLite
+    time_get_data = timeit(lambda: get_sql_data(), number=1)
     print(f"Time getting data from file: {time_get_data}")
+
+    # print size
+    print(f"shape of polars df: {watlas_df.shape}")
+    print(f'size of df: {watlas_df.estimated_size("mb")} mb')
+
+    # benchmark reading SQLite
+    time_join = timeit(lambda: join_tags(watlas_df=watlas_df), number=1)
+    print(f"Time getting data from joining tags: {time_join}")
+
+    watlas_df = join_tags(watlas_df)
+
     return 0
 
 
